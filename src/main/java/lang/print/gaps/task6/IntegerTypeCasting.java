aff8596bdd60fbc20d42fa86c174f6264318ae60
mymindinsane <arturguletskiy21@gmail.com>
2024-03-01 16:57:11 +0300

    done IntegerTypeCasting.java

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task6;
 
+import java.sql.SQLOutput;
+
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long first = 1000L;
+        int second = (int) first;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
